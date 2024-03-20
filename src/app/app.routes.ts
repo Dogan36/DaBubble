c26eb36b4152acdaf9d5c7af7fbb8a649eace7a9
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-03-20 19:33:22 +0100

    generated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainPageComponent, data: { title: 'Da Bubble | Home' } },
+    { path: 'login', component: LoginPageComponent, data: { title: 'Da Bubble | Login' } },
+    { path: 'imprint', component: ImprintComponent, data: { title: 'Da Bubble | Imprint' } },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent, data: { title: 'Da Bubble | Privacy Policy' } },
+    ];
