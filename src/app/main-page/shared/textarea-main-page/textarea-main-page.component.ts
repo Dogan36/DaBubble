cfd580d1d37ace4e9bb081c0ad0c91de9c58dccc
Regina Gering <Regina.Gering@hotmail.de>
2024-04-22 21:34:21 +0200

    Textarea validation & functionality

--- src/app/main-page/shared/textarea-main-page/textarea-main-page.component.ts
+++ src/app/main-page/shared/textarea-main-page/textarea-main-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Message } from '../../../models/message.class';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ChannelService } from '../../../services/channel.service';
 import { AuthService } from '../../../services/auth.service';
 
@@ -22,22 +22,22 @@ export class TextareaMainPageComponent {
 
   constructor(private channelService: ChannelService, private authService: AuthService){}
 
-  submitMessage() {
-    // let messageField = <HTMLInputElement>document.getElementById('message-field');
+  submitMessage(ngForm: NgForm) {
 
-    // if(messageField) {
-      // messageField.value = '';
-
+      if (ngForm.valid && this.message.message.trim() !== '') { 
       this.message.member = this.authService.uid;
       this.message.timestamp = Date.now();
 
       if(this.onChannelBoard) {
         this.channelService.startNewChat({timestamp: this.message.timestamp}, this.channelService.toJSONmessage(this.message));
+        console.log('Event hat geklappt', this.message.message);
 
       } else if(this.onThreadBoard) {
         const selectedChat = this.channelService.selectedChannelChats[this.channelService.selChatIndex];
         if(selectedChat && selectedChat.chatId) {
         this.channelService.addMessageToChat(this.channelService.toJSONmessage(this.message), selectedChat.chatId);
+
+        console.log('Event hat geklappt', this.message.message);
       }}
       // else if(this.onPrivatChat) {
 
@@ -45,18 +45,17 @@ export class TextareaMainPageComponent {
 
       // }
     // }
-    this.message.message = '';
+    ngForm.resetForm();
+      } else {
+        console.log('Ungültige Eingabe');
+      }
   }
 
 
-//   onEnterKeyPressed(event: Event) {
-//   // Überprüfe, ob die gedrückte Taste die Enter-Taste ist
-//   if ((event as KeyboardEvent).key === 'Enter' && (event as KeyboardEvent).shiftKey!) {
-//     // Verhindere das Standardverhalten des Textfelds (Umbruch einer neuen Zeile)
-//     event.preventDefault();
-
-//     // Rufe deine submitMessage-Funktion auf
-//     this.submitMessage();
-//   }
-// }
+  onEnterKeyPressed(event: Event, ngForm: NgForm) {
+  if (!(event as KeyboardEvent).shiftKey && (event as KeyboardEvent).key === 'Enter') {
+    event.preventDefault();
+    this.submitMessage(ngForm);
+  }
+}
 }
