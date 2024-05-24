1fcc5c2ccf95f35bfc1101ea56a602845067799a
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-05-24 13:12:49 +0200

    bugfix starting animation

--- src/app/starting-page/start-animation/start-animation.component.ts
+++ src/app/starting-page/start-animation/start-animation.component.ts
@@ -9,45 +9,51 @@ import { NgClass } from '@angular/common';
   templateUrl: './start-animation.component.html',
   styleUrl: './start-animation.component.scss'
 })
-export class StartAnimationComponent {
+
+
+export class StartAnimationComponent implements OnInit {
   isAnimatedOnce: boolean = false;
    
-    constructor( private renderer: Renderer2) {}
-      ngOnInit() {
-       
-       
-          this.setBodyOverflowHidden(true);
-          this.startAnimation();
-       
-    
-      }
-    startAnimation() {
-      setTimeout(() => this.animateStage1(), 1000);
-      setTimeout(() => this.animateStage2(), 3000); 
-     setTimeout(() => this.animateStage3(), 5000); 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.setBodyOverflowHidden(true);
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    setTimeout(() => this.animateStage1(), 1000);
+    setTimeout(() => this.animateStage2(), 3000); 
+    setTimeout(() => this.animateStage3(), 5000); 
     setTimeout(() => this.animateStage4(), 6000); 
-    }
-  
-    animateStage1() {
+  }
+
+  animateStage1() {
+    if (typeof document !== 'undefined') {
       const imgContainer = document.querySelector('.img-container');
       if (imgContainer) {
         this.renderer.addClass(imgContainer, 'left');
       }
-    }
-  
-    animateStage2() {
+    } 
+  }
+
+  animateStage2() {
+    if (typeof document !== 'undefined') {
       const textContainerP = document.querySelector('.text-container p');
       if (textContainerP) {
         this.renderer.addClass(textContainerP, 'right');
       }
-    }
-  
-    animateStage3() {
+    } 
+  }
+
+  animateStage3() {
+    if (typeof document !== 'undefined') {
       const logoContainer = document.querySelector('.logo-container');
       const imgContainer = document.querySelector('.img-container');
       const textContainer = document.querySelector('.text-container');
       const textContainerP = document.querySelector('.text-container p');
       const background = document.querySelector('.background');
+      
       if (logoContainer) {
         this.renderer.addClass(logoContainer, 'moveOnTop');
       }
@@ -63,24 +69,27 @@ export class StartAnimationComponent {
       if (background) {
         this.renderer.addClass(background, 'opacity');
       }
-    }
-  
-    animateStage4() {
-      this.setBodyOverflowHidden(false);
+    } 
+  }
+
+  animateStage4() {
+    this.setBodyOverflowHidden(false);
    
+    if (typeof document !== 'undefined') {
       const background = document.querySelector('.background');
       if (background) {
         this.renderer.addClass(background, 'd-none');
       }
-    }
-  
-    setBodyOverflowHidden(hidden: boolean) {
+    } 
+  }
+
+  setBodyOverflowHidden(hidden: boolean) {
+    if (typeof document !== 'undefined') {
       if (hidden) {
         this.renderer.setStyle(document.body, 'overflow', 'hidden');
       } else {
         this.renderer.removeStyle(document.body, 'overflow');
       }
-    }
-  
-  
+    } 
   }
+}
